513d637dbd0f9c26640a0d616d6c691dc3265664
Kraichik <romanfjxxxh@gmail.com>
2022-12-03 15:29:40 +0200

    Add project files.

---
+++ Highest ang Lowest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Highest_ang_Lowest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+
+        }
+        // MyMestod
+        public static string HighAndLow(string numbers)
+        {
+            string[] number = numbers.Split(new char[] { ' ' });
+
+            int min = int.Parse(number[0]);
+            int max = int.Parse(number[0]);
+            foreach (string num in number)
+            {
+                if (min > int.Parse(num))
+                    min = int.Parse(num);
+
+                if (max < int.Parse(num))
+                    max = int.Parse(num);
+            }
+
+            return $"{max} {min}";
+        }
+        // More Clever method
+        public static string HighAndLow1(string numbers)
+        {
+            var parsed = numbers.Split().Select(int.Parse);
+            return parsed.Max() + " " + parsed.Min();
+        }
+        
+    }
+}
